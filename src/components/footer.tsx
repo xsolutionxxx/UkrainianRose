e9949622ae82e4ca0d15588ccf336b1a606d13bf
Nazar Talaievych <losiara345@gmail.com>
2024-06-13 22:57:40 +0300

    navbar/footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,52 +1,89 @@
 import MaxWidthWrapper from "./MaxWidthWrapper";
 import Link from 'next/link';
+import { ChevronRight, Mail, Facebook, Instagram } from 'lucide-react';
+import { Button, buttonVariants } from './ui/button';
 
 export default function Footer() {
 
     const links = [
-        { text: 'Про нас', href: '/about' },
-        { text: 'Оплата і доставка', href: '/payment-and-delivery' },
-        { text: 'Обмін і повернення', href: '/exchange-and-return' },
-        { text: 'Публічна оферта', href: '/public-offer' },
-        { text: 'Контакти', href: '/contacts' },
+        {
+            title: 'Про нас',
+            href: '/about'
+        },
+        {
+            title: 'Оплата і доставка',
+            href: '/payment-and-delivery'
+        },
+        {
+            title: 'Обмін і повернення',
+            href: '/exchange-and-return'
+        },
+        {
+            title: 'Публічна оферта',
+            href: '/public-offer'
+        },
+        {
+            title: 'Контакти',
+            href: '/contacts'
+        },
     ];
 
     const contacts = [
-        { name: 'number', text: '+38 (050) 434 71 02', href: 'tel:+380504347102' },
-        { name: 'email', text: 'info@ukrainianrose.com', href: 'mailto:info@ukrainianrose.com' },
-        { name: 'schedule', text: 'Щоденно з 10:00 до 18:00', href: '/contacts' }
+        {
+            name: 'number',
+            text: '+38 (050) 434 71 02',
+            href: 'tel:+380504347102'
+        },
+        {
+            name: 'email',
+            text: 'info@ukrainianrose.com',
+            href: 'mailto:info@ukrainianrose.com'
+        },
+        {
+            name: 'schedule',
+            text: 'Щоденно з 10:00 до 18:00',
+            href: '/contacts'
+        }
     ]
 
     const socials = [
-        { src: '/instagram.svg', alt: 'instagram', href: 'https://www.instagram.com/ukrainianrose/' },
-        { src: '/facebook.svg', alt: 'facebook', href: 'https://www.facebook.com/ukrainianrose/' }
+        {
+            name: 'Instagram',
+            media: <Instagram strokeWidth={1.25} className="w-7 h-7 transition-all duration-200 transform hover:scale-105" />,
+            href: 'https://www.instagram.com/ukrainianrose/'
+        },
+        {
+            name: 'Facebook',
+            media: <Facebook strokeWidth={1.25} className="w-7 h-7 transition-all duration-200 transform hover:scale-105" />,
+            href: 'https://www.facebook.com/ukrainianrose/'
+        }
     ];
 
     return (
         <>
-            <footer className="bg-custom-dark-blue py-6 px-8 lg:py-8 lg:px-16">
-                <MaxWidthWrapper className="relative grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+            <footer className="bg-custom-dark-blue px-8 py-6 lg:px-16 lg:py-8">
+                <MaxWidthWrapper className="relative grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
                     <div className="flex flex-col gap-3">
-                        <h2 className="text-white text-lg font-base w-4/5">Дізнайтесь більше про те, що Вас цікавить</h2>
-                        <ul className="pl-2 w-full flex flex-col justify-start gap-2 font-light text-base text-white capitalize">
+                        <h2 className="w-4/5 font-base text-lg text-white">Дізнайтесь більше про те, що Вас цікавить</h2>
+                        <ul className="w-full flex flex-col justify-start gap-2 font-light text-base text-white capitalize md:pl-2">
                             {links.map((link) => (
-                                <li key={link.text} className="hover:ml-1 transition-all duration-100">
+                                <li key={link.title} className="inline transition-all duration-100 hover:ml-1">
                                     <Link
                                         href={link.href}
                                         className="relative flex gap-2 hover:text-red-500">
-                                        <img
-                                            src="/chevron-right.svg"
-                                            alt="chevron-right" />
-                                        {link.text}
+                                        <ChevronRight
+                                            strokeWidth={1.25}
+                                            className="w-6 h-6" />
+                                        {link.title}
                                     </Link>
                                 </li>
                             ))}
                         </ul>
                     </div>
-                    <div className="flex flex-col gap-6">
+                    <div className="flex flex-col gap-6 sm:gap-10">
                         <div className="flex flex-col gap-3">
-                            <h2 className="text-white text-lg font-base w-4/5">Потрібна допомога? Зв'яжіться з нами</h2>
-                            <ul className="pl-6 list-disc w-full flex flex-col justify-start gap-2 font-light text-sm text-white">
+                            <h2 className="w-4/5 font-base text-lg text-white">Потрібна допомога? Зв'яжіться з нами</h2>
+                            <ul className="pl-6 w-full flex flex-col justify-start gap-2 font-light text-sm text-white list-disc">
                                 {contacts.map((contact) => (
                                     <li key={contact.name}>
                                         <Link
@@ -58,39 +95,42 @@ export default function Footer() {
                                 ))}
                             </ul>
                         </div>
-                        <div className="flex flex-col gap-5">
-                            <h2 className="text-white text-lg font-base w-4/5">Слідкуйте за нами в соціальних мережах</h2>
+                        <div className="flex flex-col gap-5 ">
+                            <h2 className="w-4/5 font-base text-lg text-white">Слідкуйте за нами в соціальних мережах</h2>
                             <ul className="pl-3 w-full flex justify-start gap-4 font-light text-base text-white capitalize">
                                 {socials.map((social) => (
-                                    <li key={social.alt}>
+                                    <li key={social.name}>
                                         <Link
                                             href={social.href}
                                             target="_blank"
                                             rel="noopener noreferrer" className="relative flex gap-2">
-                                            <img src={social.src} alt={social.alt} className="w-6 h-6 transition-all duration-75 transform hover:scale-105" />
+                                            {social.media}
                                         </Link>
                                     </li>
                                 ))}
                             </ul>
                         </div>
                     </div>
-                    <div className="flex flex-col gap-3 md:grid-cols-1">
+                    <div className="flex flex-col gap-3 sm:w-2/3 sm:col-span-2 md:w-full md:col-span-1">
                         <div className="w-full">
-                            <h2 className="text-white text-lg font-base">Підпишіться на розсилку</h2>
-                            <p className="text-white text-sm font-light">Отримуйте останні новини та оновлення прямо на вашу електронну пошту.</p>
+                            <h2 className="font-base text-lg text-white">Підпишіться на розсилку</h2>
+                            <p className="font-light text-sm text-white">Отримуйте останні новини та оновлення прямо на вашу електронну пошту.</p>
                         </div>
                         <input
                             type="text"
                             name="email"
                             placeholder="Ваша електронна пошта"
-                            className="py-3 px-5 w-full bg-custom-deep-blue border rounded-lg border-slate-600 text-sm text-white focus:outline-none" />
-                        <button
-                            type="submit"
-                            className="py-2 px-4 mx-auto mr-0 w-full lg:w-1/2 lg:mx-0 bg-custom-dark-blue border rounded-lg border-slate-600 text-sm font-light text-white hover:text-red-500 hover:bg-custom-deep-blue hover:border-red-500">Підписатися</button>
+                            className="px-5 py-3 w-full bg-custom-deep-blue border rounded-lg border-slate-600 text-sm text-white focus:outline-none" />
+                        <Button
+                            className={`${buttonVariants({ size:'lg', variant: 'rose'})} font-light text-sm text-white sm:w-1/2 md:w-3/5 lg:w-2/3`}>
+                            <Mail
+                                strokeWidth={1}
+                                className="mr-2 w-5" /> Підписатися
+                        </Button>
                     </div>
                 </MaxWidthWrapper>
-                <p className="mt-14 text-slate-600 text-xs font-base md:text-sm md:font-normal text-center">All rights reserved © 2024 Ukrainian Rose</p>
-            </footer>
+                <p className="mt-10 text-center font-base text-xs text-slate-600 md:mt-14 md:font-normal md:text-sm">All rights reserved © 2024 Ukrainian Rose</p>
+            </footer >
         </>
     );
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,14 +1,17 @@
 "use client";
 
 import { useState, useEffect } from 'react';
-import MaxWidthWrapper from "../components/MaxWidthWrapper";
+import MaxWidthWrapper from "./MaxWidthWrapper";
 import Link from 'next/link';
-import { title } from 'process';
+import { Menu, Search, X, ArrowLeft, Heart, ShoppingBag, UserRound, ChevronRight, LayoutDashboard } from 'lucide-react';
 
 export default function Navbar() {
     const [navOpen, setNavOpen] = useState(false);
     const [searchOpen, setSearchOpen] = useState(false);
     const [currentPhraseIndex, setCurrentPhraseIndex] = useState(0);
+
+    const user = undefined;
+    const isAdmin = false;
 
     const navLinks = [
         { title: 'Колекція', href: '/collection' },
@@ -27,21 +30,9 @@ export default function Navbar() {
         </>,
         <>
             <p>
-                Подаруйте своїй шкірі <span className="font-bold text-red-500">ніжність</span> і <span className="font-bold text-red-500">свіжість</span> троянд!
-            </p>
-        </>,
-        <>
-            <p>
-                Ваша краса в <span className="font-bold text-red-500">гармонії з природою:</span> косметика з троянд для бездоганного догляду.
-            </p>
-        </>,
-        <>
-            <p>
                 Квіти троянд у кожному флаконі - <span className="font-bold text-red-500">якісна</span> косметика для вашої краси.
             </p>
-        </>,
-
-
+        </>
     ];
 
     const toggleNavbar = () => {
@@ -62,94 +53,126 @@ export default function Navbar() {
     }, []);
 
     return (
-        <>
-            <nav className="relative">
-                <div className={`${searchOpen || navOpen ? 'hidden lg:block' : ''}`}>
-                    <MaxWidthWrapper className="py-1.5 text-center flex justify-center items-center font-regular text-xs text-custom-dark-blue md:text-sm lg:text-base overflow-x-hidden whitespace-nowrap">
-                        <div className="inline-block">
-                            {phrases[currentPhraseIndex]}
-                        </div>
-                    </MaxWidthWrapper>
-                    <div className="bg-custom-dark-blue">
-                        <MaxWidthWrapper>
-                            <div className="relative px-4 py-6 flex justify-between items-center lg:px-3 lg:h-24 xl:px-2">
-                                <div className="flex items-center gap-4 md:gap-6">
-                                    <button onClick={toggleNavbar} className="w-5 md:w-6 lg:hidden cursor-pointer">
-                                        <img src="/menu.svg" alt="menu" className="w-full cursor-pointer" />
-                                    </button>
+        <nav className="relative">
+            <div className={`${searchOpen || navOpen ? 'hidden lg:block' : ''}`}>
+                <MaxWidthWrapper className="py-1.5 flex justify-center items-center text-center font-regular text-xs text-custom-dark-blue md:text-sm lg:text-base overflow-x-hidden whitespace-nowrap">
+                    <div className="inline-block">
+                        {phrases[currentPhraseIndex]}
+                    </div>
+                </MaxWidthWrapper>
+                <div className="bg-custom-dark-blue">
+                    <MaxWidthWrapper>
+                        <div className="relative px-4 py-6 flex justify-between items-center lg:px-3 lg:h-24 xl:px-2">
+                            <div className="flex items-center gap-4 md:gap-6">
 
-                                    <button onClick={toggleSearch} className="w-5 md:w-6 lg:hidden cursor-pointer">
-                                        <img src="/search.svg" alt="search" className="w-full cursor-pointer" />
-                                    </button>
+                                {/* Burger-menu Mobile*/}
+                                <button onClick={toggleNavbar} className="w-6 lg:hidden cursor-pointer">
+                                    <Menu
+                                        strokeWidth={1.25}
+                                        className="text-white full-size" />
+                                </button>
 
-                                    {/* SearchBar DESKTOP */}
-                                    <div className={`relative hidden lg:flex bg-custom-dark-blue bg-transparent ${searchOpen ? 'py-3 h-16' : 'delay-1000 py-0 h-0'}`}>
-                                        <div className={`relative flex ml-9 ease-in-out duration-1000 ${searchOpen ? 'w-64' : 'w-0'}`}>
-                                            <input
-                                                type="text"
-                                                name="search"
-                                                placeholder="Type to search..."
-                                                className={`bg-white border rounded-full border-slate-600 text-custom-dark-blue text-base focus:outline-none placeholder-slate-600 ease-in-out duration-300 ${searchOpen ? 'py-2 pl-5 pr-10 w-full opacity-100 delay-200' : 'w-0 lg:delay-500 opacity-0'}`} />
-                                            <button
-                                                className={`absolute flex items-center justify-center h-8 w-8 top-1/2 right-1.5 transform -translate-y-1/2 rounded-full z-10 ease-in-out duration-1000 ${searchOpen ? 'bg-custom-dark-blue opacity-100' : 'opacity-100 delay-1000'}`}>
-                                                <img src="/search.svg" alt="search" className="cursor-pointer" />
-                                            </button>
-                                        </div>
+                                {/* SearchBar Mobile*/}
+                                <button onClick={toggleSearch} className="w-6 lg:hidden cursor-pointer">
+                                    <Search
+                                        strokeWidth={1.25}
+                                        className="text-white full-size" />
+                                </button>
+
+                                {/* SearchBar DESKTOP */}
+                                <div className={`relative hidden bg-transparent lg:flex ${searchOpen ? 'py-2 h-16' : 'py-0 h-0 delay-1000'}`}>
+                                    <div className={`relative ml-9 flex ease-in-out duration-1000 ${searchOpen ? 'w-64' : 'w-0'}`}>
+                                        <input
+                                            type="text"
+                                            name="search"
+                                            placeholder="Type to search..."
+                                            className={`bg-white border rounded-full border-slate-600 text-base text-custom-dark-blue ease-in-out duration-300 placeholder-slate-600 focus:outline-none ${searchOpen ? 'py-2 pl-5 pr-12 w-full opacity-100 delay-200' : 'w-0 lg:delay-500 opacity-0'}`} />
                                         <button
-                                            onClick={toggleSearch}
-                                            className={`absolute h-8 w-8 z-20 top-1/2 transform -translate-y-1/2 transition-opacity duration-500 delay-300 ${searchOpen ? 'opacity-100' : 'delay-0 opacity-0'}`}>
-                                            <img src="/close.svg" alt="close" className="w-full cursor-pointer" />
+                                            className={`absolute top-1/2 right-1.5 h-9 w-9 flex items-center justify-center rounded-full z-10 transform -translate-y-1/2 ease-in-out duration-1000 ${searchOpen ? 'bg-custom-dark-blue opacity-100' : 'opacity-100 delay-1000'}`}>
+                                            <Search
+                                                strokeWidth={1.25}
+                                                className="w-7 h-7 text-white" />
                                         </button>
                                     </div>
-                                </div>
-                                <div className="absolute left-1/2 transform -translate-x-1/2 w-2/6 md:w-1/4 ">
-                                    <img src="/logo.svg" alt="logo" className="w-full cursor-pointer" />
-                                </div>
-                                <div className="flex gap-4 md:gap-6 lg:gap-8">
-                                    <div className="w-5 md:w-6">
-                                        <img src="/heart.svg" alt="heart" className="w-full cursor-pointer" />
-                                    </div>
-                                    <div className="relative w-5 md:w-6">
-                                        <img src="/shopping-bag.svg" alt="shopping bag" className="w-full cursor-pointer" />
-                                        <div className="absolute w-custom-width-18 h-custom-height-18 -bottom-2 -right-2 flex justify-center items-center bg-white lg:w-5 lg:h-5 lg:-bottom-2.5 lg:-right-2.5  rounded-full text-center text-xs text-custom-dark-blue lg:text-sm overflow-hidden">99</div>
-                                    </div>
-                                    <div className="w-5 md:w-6">
-                                        <img src="/user.svg" alt="user" className="w-full cursor-pointer" />
-                                    </div>
+                                    <button
+                                        onClick={toggleSearch}
+                                        className={`absolute top-1/2 h-8 w-8 z-20 transform -translate-y-1/2 transition-opacity duration-500 delay-300 ${searchOpen ? 'opacity-100' : 'opacity-0 delay-0'}`}>
+                                        <X strokeWidth={1.25} className="full-size text-white " />
+                                    </button>
                                 </div>
                             </div>
-                        </MaxWidthWrapper>
-                    </div>
-                </div>
 
-                <div className="hidden lg:block h-custom-height-1 bg-slate-600" />
+                            {/* Logo */}
+                            <Link href="/" className="absolute w-2/6 left-1/2 transform -translate-x-1/2 md:w-1/4 ">
+                                <img src="/logo.svg" alt="logo" className="w-full cursor-pointer" />
+                            </Link>
 
-                <div className="bg-custom-deep-blue">
-                    <MaxWidthWrapper>
-                        <div className={`fixed w-full z-50 lg:flex bg-custom-deep-blue lg:static lg:justify-around lg:items-center lg:flex-row ${navOpen || searchOpen ? 'block' : 'hidden'}`}>
-                            <div className="realative px-6 py-2 w-full h-16 flex items-center bg-custom-dark-blue lg:hidden">
-                                <button className="mr-5 w-10">
-                                    {navOpen ? <img src="/close.svg" alt="close" onClick={toggleNavbar} className="w-full cursor-pointer" /> : <img src="/arrow-left.svg" alt="arrow-left" onClick={toggleSearch} className="w-full cursor-pointer" />}
-                                </button>
-                                <input type="text" name="search" placeholder="Type to search..." className="py-2 pl-5 pr-10 w-full h-full bg-custom-deep-blue border rounded-full border-slate-600 text-lg text-white focus:outline-none" />
-                                <button className="absolute right-9 w-6 md:w-7">
-                                    <img src="/search.svg" alt="search" className="w-full cursor-pointer" />
-                                </button>
-                            </div>
-                            <ul className={`realative px-6 py-5 w-full flex flex-col gap-5 lg:flex-row lg:justify-around lg:items-center font-light text-2xl text-white capitalize lg:text-xl ${navOpen ? 'block' : 'hidden lg:flex'}`}>
-                                {navLinks.map((navLink) => (
-                                    <li key={navLink.title}>
-                                        <Link href={navLink.href} className="relative flex transition-all hover:text-red-500">
-                                            {navLink.title}
-                                            <img src="/chevron-right.svg" alt="chevron-right" className="absolute top-0.5 right-0 lg:hidden" />
+                            {/* Whishlist, cart, account */}
+                            <div className="flex gap-4 md:gap-6 lg:gap-8">
+                                <Link href="/wishlist" className="w-6 text-white cursor-pointer lg:w-7">
+                                    <Heart strokeWidth={1.25} className="full-size" />
+                                </Link>
+                                <Link href="/cart" className="relative w-6 lg:w-7 text-white cursor-pointer">
+                                    <ShoppingBag strokeWidth={1.25} className="full-size" />
+                                    <div className="absolute w-custom-width-18 h-custom-height-18 -bottom-2 -right-2 flex justify-center items-center bg-white lg:w-5 lg:h-5 lg:-bottom-2.5 lg:-right-2.5  rounded-full text-center text-xs text-custom-dark-blue lg:text-sm overflow-hidden">99</div>
+                                </Link>
+                                {user ? (
+                                    <>
+                                        <Link href="/account" className="w-6 lg:w-7 text-white cursor-pointer">
+                                            <UserRound strokeWidth={1.25} className="full-size" />
                                         </Link>
-                                    </li>
-                                ))}
-                            </ul>
+                                        {isAdmin ?
+                                            <Link href="/dashboard" className="w-6 lg:w-7 text-white cursor-pointer">
+                                                <LayoutDashboard strokeWidth={1.25} className="full-size" />
+                                            </Link>
+                                            : null}
+                                    </>
+                                ) : (
+                                    <>
+                                        <Link href="/api/auth/login" className="w-6 lg:w-7 text-white cursor-pointer">
+                                            <UserRound strokeWidth={1.25} className="full-size" />
+                                        </Link>
+                                    </>
+                                )}
+                            </div>
                         </div>
                     </MaxWidthWrapper>
                 </div>
-            </nav >
-        </>
+            </div>
+
+            {/* Divider */}
+            <div className="hidden lg:block h-custom-height-1 bg-slate-600" />
+
+            {/* Navbar DESKTOP, MOBILE and Search MOBILE functionality */}
+            <div className="bg-custom-deep-blue">
+                <MaxWidthWrapper>
+                    <div className={`fixed w-full bg-custom-deep-blue z-50 lg:static lg:flex lg:flex-row lg:justify-around lg:items-center ${navOpen || searchOpen ? 'block' : 'hidden'}`}>
+
+                        {/* SearchBar MOBILE */}
+                        <div className="realative px-6 py-2 w-full h-16 flex items-center bg-custom-dark-blue lg:hidden">
+                            <button className="mr-5 w-8 text-white cursor-pointer">
+                                {navOpen ? <X strokeWidth={1} onClick={toggleNavbar} className="full-size" /> : <ArrowLeft strokeWidth={1} onClick={toggleSearch} className="full-size" />}
+                            </button>
+                            <input type="text" name="search" placeholder="Type to search..." className="py-2 pr-10 pl-5 full-size bg-custom-deep-blue border rounded-full border-slate-600 text-lg text-white focus:outline-none" />
+                            <button className="absolute right-9 w-6 text-white cursor-pointer md:w-7">
+                                <Search strokeWidth={1.5} className="full-size" />
+                            </button>
+                        </div>
+
+                        {/* Navbar DESKTOP, MOBILE */}
+                        <ul className={`realative px-6 py-5 w-full flex flex-col gap-5 lg:flex-row lg:justify-around lg:items-center font-light text-2xl text-white capitalize lg:pb-0 lg:text-xl ${navOpen ? 'block' : 'hidden lg:flex'}`}>
+                            {navLinks.map((navLink) => (
+                                <li key={navLink.title}>
+                                    <Link href={navLink.href} className="relative flex transition-all duration-200 hover:text-red-500 lg:pb-5 lg:link-ease-in-out ">
+                                        {navLink.title}
+                                        <ChevronRight strokeWidth={1.25} className="absolute right-0 w-8 h-full lg:hidden" />
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </MaxWidthWrapper>
+            </div>
+        </nav >
     );
 }
